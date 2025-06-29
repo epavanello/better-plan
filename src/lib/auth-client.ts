5158bf1960aec650e8078ff7bff0694553ff7e80
Emanuele Pavanello <pavanello.emanuele@gmail.com>
2025-06-29 22:02:03 +0200

    fix

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,8 @@
 import { organizationClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
+import { envConfig } from "./env"
 
 export const authClient = createAuthClient({
     plugins: [organizationClient()],
-    baseURL: "http://localhost:3000"
+    baseURL: envConfig.APP_URL
 })
